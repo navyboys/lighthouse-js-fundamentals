3d05cd4e239fe33d36c7a8f7b0a93487da917e3a
Ben Zheng <navyboys@gmail.com>
2016-10-07 11:07:01 -0700

    change loopy lighthouse to fucntion

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,11 +1,15 @@
-for (var i = 100; i <= 200; i++) {
-  if (i % 12 === 0) {
-    console.log('LoopyLighthouse');
-  } else if (i % 3 === 0) {
-    console.log('Loopy');
-  } else if (i % 4 === 0) {
-    console.log('Lighthouse');
-  } else {
-    console.log(i);
+function loopyLighthouse(range, multiples, words) {
+  for (var i = range[0]; i <= range[1]; i++) {
+    if (i % (multiples[0] * multiples[1]) === 0) {
+      console.log(words[0] + words[1]);
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
   }
 }
+
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
